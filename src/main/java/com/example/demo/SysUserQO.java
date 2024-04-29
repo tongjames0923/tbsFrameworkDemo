e4446ae008deded5bf900162f87e3f7b73f74e49
abstergo <tongjames@live.com>
2024-04-29 14:12:08 +0800

    提取方法 新增in和not in操作符 支持对遍历器的值转换和一些简单的判空 完善支持字段序号排序

--- src/main/java/com/example/demo/SysUserQO.java
+++ src/main/java/com/example/demo/SysUserQO.java
@@ -1,47 +1,47 @@
 package com.example.demo;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import tbs.framework.sql.annotations.OrField;
 import tbs.framework.sql.annotations.QueryField;
+import tbs.framework.sql.annotations.QueryIndex;
 import tbs.framework.sql.annotations.QueryOrderField;
 import tbs.framework.sql.enums.QueryConnectorEnum;
 import tbs.framework.sql.enums.QueryContrastEnum;
 import tbs.framework.sql.interfaces.IQuery;
 
 import java.io.Serializable;
+import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SysUserQO implements IQuery, Serializable {
 
     private static final long serialVersionUID = -6441511924462004046L;
-
-    public SysUserQO(String nameOrPhone, String password, Integer sex, Long id) {
-        this.nameOrPhone = nameOrPhone;
-        this.password = password;
-        this.sex = sex;
-        this.id = id;
-    }
-
-    public SysUserQO() {
-    }
 
     @Override
     public String baseQuerySql() {
         return "select * from sys_user ";
     }
 
-    @QueryField(map = "name", connector = QueryConnectorEnum.OR, contrast = QueryContrastEnum.LLIKE)
-    @QueryField(map = "phone", connector = QueryConnectorEnum.OR, contrast = QueryContrastEnum.LLIKE)
+    @QueryField(map = "name", index = 1, connector = QueryConnectorEnum.OR, contrast = QueryContrastEnum.LLIKE)
+    @QueryField(map = "phone", index = 0, connector = QueryConnectorEnum.OR, contrast = QueryContrastEnum.LLIKE)
+    @QueryIndex(index = 4)
     private String nameOrPhone;
 
     @QueryField
+    @QueryIndex(index = 3)
     private String password;
 
     @QueryField
+    @QueryIndex(index = 2)
     private Integer sex;
 
-    @QueryField
+    @QueryField(contrast = QueryContrastEnum.IN)
     @OrField
     @QueryOrderField
-    private Long id;
+    @QueryIndex(index = 1)
+    private List<Long> id;
 }
